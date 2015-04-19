18a1bc2ed5f1efd1b9b7fc714c4ab5417ef99561
Daniele Megna <megna.dany@gmail.com>
2015-04-19 10:06:46 +0200

    Refactoring introducing a map..

--- src/clojure_fizzbuzz/core.clj
+++ src/clojure_fizzbuzz/core.clj
@@ -11,12 +11,15 @@
 )
 
 (defn fizzbuzz [number]
+  
+  (def dictionary {3 "Fizz" 5 "Buzz"})
+
   (if (and (= 0 (rem number 3)) (= 0 (rem number 5)))
     "FizzBuzz"
     (if (= 0 (rem number 3))
-      "Fizz"
+      (dictionary 3)
       (if (= 0 (rem number 5))
-        "Buzz"
+        (dictionary 5)
         (str number)
       )
     )
